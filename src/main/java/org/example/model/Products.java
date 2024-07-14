1dc47bde926edd15fb043666b04caa535f8cf1ff
shekhovtsovsergey <89629417575@mail.ru>
2024-07-14 09:22:07 +0300

    Lesson10

---
+++ src/main/java/org/example/model/Products.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+public class Products {
+   private int id;
+   private String name;
+   private String description;
+   private int price;
+   private int stock;
+
+}
--- src/main/resources/db/migration/V1__schema_migration.sql
+++ src/main/resources/db/migration/V1__schema_migration.sql
@@ -3,6 +3,14 @@ CREATE TABLE users (
                        username VARCHAR(255)
 );
 
+CREATE TABLE products (
+                                 id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                 user_id BIGINT,
+                                 account_number VARCHAR(255),
+                                 balance DECIMAL(10, 2),
+                                 product_type VARCHAR(255),
+                                 FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 
 SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC';
